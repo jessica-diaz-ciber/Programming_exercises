abcbcba6ade0ee99722ff42d2c2358b8b14c8e38
CUCUxii <96772264+CUCUxii@users.noreply.github.com>
2023-02-11 20:47:18 +0100

    Delete Item.java

--- HolocaustoH/Item.java
+++
@@ -1,8 +0,0 @@
-package HolocaustoH;
-
-public class Item extends ObjetoJuego{
-	private int peso;
-	public Item() {}
-	public int getPeso() { return peso;}
-	public void setPeso(int peso) { this.peso = peso;}
-}
